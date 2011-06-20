28560bc37400e3c2ae42d47028ea2894d2053731
LenardG <LenardG@localhost>
2011-06-20 18:01:26 +0300

    Imported first version

---
+++ WSDLMerge/WSDLMerger.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+using System.Net;
+
+namespace WSDLMerge
+{
+    internal static class WSDLMerger
+    {
+        public const string WSDLNamespace = "http://schemas.xmlsoap.org/wsdl/";
+
+        public const string XSDNamespace = "http://www.w3.org/2001/XMLSchema";
+
+        /// <summary>
+        /// Merge WSDL and XSD in filename and write it to destination
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <param name="destination"></param>
+        public static void Merge ( string filename, string destination )
+        {
+            try
+            {
+                // load WSDL
+                XmlDocument wsdl = new XmlDocument ();
+                wsdl.Load ( filename );
+
+                // add standard namespaces
+                XmlNamespaceManager manager = PrepareNamespaceManager ( wsdl );
+
+                // verify it is a WSDL file
+                if ( !VerifyWSDL ( wsdl, manager ) )
+                {
+                    Console.WriteLine ( "Error: Does not seem to be a WSDL file!" );
+                    return;
+                }
+
+                // we need the types element so either look it up or create it
+                XmlElement typesElement = CreateOrFindTypes ( wsdl, manager );
+                if ( typesElement == null )
+                {
+                    Console.WriteLine ( "Error: definitions/types cannot be found nor created!" );
+                    return;
+                }
+
+                // process all imports recursively
+                ProcessImports ( filename, wsdl, typesElement, manager );
+
+                // write result
+                Console.WriteLine ( "Saving merged WSDL" );
+                wsdl.Save ( destination );
+            }
+            catch ( Exception ex )
+            {
+                Console.WriteLine ( ex.ToString () );
+            }
+        }
+
+        /// <summary>
+        /// Prepare namespace manager for usage (add standard namespaces)
+        /// </summary>
+        /// <param name="wsdl"></param>
+        /// <returns></returns>
+        private static XmlNamespaceManager PrepareNamespaceManager ( XmlDocument wsdl )
+        {
+            XmlNamespaceManager manager = new XmlNamespaceManager ( wsdl.NameTable );
+            manager.AddNamespace ( "wsdl", WSDLNamespace );
+            manager.AddNamespace ( "xsd", XSDNamespace );
+            return manager;
+        }
+
+        /// <summary>
+        /// Verify the file is a WSDL
+        /// </summary>
+        /// <param name="wsdl"></param>
+        /// <param name="manager"></param>
+        /// <returns></returns>
+        private static bool VerifyWSDL ( XmlDocument wsdl, XmlNamespaceManager manager )
+        {
+            XmlNode node = wsdl.SelectSingleNode ( "/wsdl:definitions", manager );
+            if ( node == null )
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Find the types element from the WSDL file
+        /// </summary>
+        /// <param name="wsdl"></param>
+        /// <param name="manager"></param>
+        /// <returns></returns>
+        private static XmlElement CreateOrFindTypes ( XmlDocument wsdl, XmlNamespaceManager manager )
+        {
+            XmlNode node = wsdl.SelectSingleNode ( "/wsdl:definitions/wsdl:types", manager );
+            if ( node != null )
+            {
+                return node as XmlElement;
+            }
+
+            XmlElement types = wsdl.CreateElement ( "wsdl", "types", WSDLNamespace );
+
+            XmlNode import = wsdl.SelectSingleNode ( "/wsdl:definitions/wsdl:import", manager );
+            if ( import == null )
+            {
+                wsdl.DocumentElement.InsertBefore ( types, wsdl.DocumentElement.FirstChild );
+            }
+            else
+            {
+                wsdl.DocumentElement.InsertAfter ( types, import );
+            }
+
+            return types;
+        }
+
+        /// <summary>
+        /// Process all types elements
+        /// </summary>
+        /// <param name="wsdl"></param>
+        /// <param name="typesElement"></param>
+        /// <param name="manager"></param>
+        private static void ProcessImports ( string filename, XmlDocument wsdl, XmlElement typesElement, XmlNamespaceManager manager )
+        {
+            Dictionary<string, XmlElement> schemas = new Dictionary<string, XmlElement> ();
+
+            XmlElement schemaElement = typesElement.SelectSingleNode ( "xsd:schema", manager ) as XmlElement;
+
+            ProcessSchema ( filename, wsdl, schemaElement, manager, schemas, 0 );
+
+            XmlNode schemaNode = typesElement.SelectSingleNode ( "xsd:schema", manager );
+            typesElement.RemoveChild ( schemaNode );
+
+            foreach ( var schema in schemas.Values )
+            {
+                typesElement.AppendChild ( schema );
+            }
+        }
+
+        private static void ProcessSchema ( 
+            string filename, 
+            XmlDocument wsdl, 
+            XmlElement rootElement, 
+            XmlNamespaceManager manager, 
+            Dictionary<string, XmlElement> schemas, 
+            int level )
+        {
+            XmlNodeList imports;
+            imports = rootElement.SelectNodes ( "xsd:import", manager );
+
+            foreach ( XmlNode node in imports )
+            {
+                string importNamespace, importLocation;
+                GetImportDetails ( node, filename, out importNamespace, out importLocation );
+
+                if ( schemas.ContainsKey ( importNamespace ) ) continue;
+
+                if ( importLocation == null ) throw new InvalidOperationException ();
+
+                Console.WriteLine ( "Importing namespace: {0}", importNamespace );
+                Console.WriteLine ( "  from file: {0}", importLocation );
+
+                XmlDocument schemaDocument = new XmlDocument ();
+                schemaDocument.Load ( importLocation );
+
+                XmlElement newSchema = wsdl.ImportNode ( schemaDocument.DocumentElement, true ) as XmlElement;
+                XmlNodeList newImports = newSchema.SelectNodes ( "/xsd:import", manager );
+                foreach ( XmlNode importNode in newImports )
+                {
+                    if ( level == 0 )
+                    {
+                        newSchema.RemoveChild ( importNode );
+                    }
+                    else
+                    {
+                        if ( importNode.Attributes["schemaLocation"] != null )
+                        {
+                            importNode.Attributes.RemoveNamedItem ( "schemaLocation" );
+                        }
+                    }
+                }
+                schemas.Add ( importNamespace, newSchema );
+
+                ProcessSchema (
+                    importLocation,
+                    wsdl,
+                    schemaDocument.DocumentElement,
+                    PrepareNamespaceManager ( schemaDocument ),
+                    schemas,
+                    level + 1 );
+            }
+        }
+
+        /// <summary>
+        /// Get information about an imported XSD file
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="filename"></param>
+        /// <param name="importNamespace"></param>
+        /// <param name="importLocation"></param>
+        private static void GetImportDetails ( 
+            XmlNode node, 
+            string filename, 
+            out string importNamespace, 
+            out string importLocation )
+        {
+            if ( node is XmlElement )
+            {
+                XmlElement importElement = node as XmlElement;
+
+                importNamespace = importElement.Attributes["namespace"].Value;
+
+                if ( importElement.Attributes["schemaLocation"] != null )
+                {
+                    string location = importElement.Attributes["schemaLocation"].Value;
+
+                    // check if this is a url
+                    if (Uri.IsWellFormedUriString(location, UriKind.Absolute))
+                    {
+                        // location does not change
+                        importLocation = location;
+                    }
+                    // otherwise try to open it as a file
+                    else 
+                    {
+                        // create relative path
+                        importLocation = Path.Combine(Path.GetDirectoryName(filename), location);
+                        if (File.Exists(importLocation))
+                        {
+                            importLocation = ResolveRelativeFilePath(importLocation);
+                        }
+                        else
+                        {
+                            throw new InvalidOperationException("Could not load schema");
+                        }
+                    }
+                }
+                else
+                {
+                    importLocation = null;
+                }
+            }
+            else
+            {
+                throw new InvalidOperationException ();
+            }
+        }
+
+        /// <summary>
+        /// Remove any .. from in between the path
+        /// </summary>
+        /// <param name="importLocation"></param>
+        /// <returns></returns>
+        private static string ResolveRelativeFilePath(string importLocation)
+        {
+            FileInfo f = new FileInfo(importLocation);
+            importLocation = f.FullName;
+            return importLocation;
+        }
+
+
+    }
+}
